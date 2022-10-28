167e591c0eb741b0c068be9e1e67aae6296a7523
Kirill <k.novoselov@g.nsu.ru>
2022-10-28 19:23:44 +0700

    сделал небольшие корректировки

--- draw.py
+++ draw.py
@@ -4,13 +4,16 @@ import numpy as np
 nAdaptation = 6
 nDots = 100
 dat = []
-with open(r"C:\Users\Кирилл\Desktop\freefem_test\DATA\No adaptation.txt") as f:
+with open(r".\DATA\No adaptation.txt") as f:
     for line in f:
         dat.append([float(x) for x in line.split()])
 for i in range(1, nAdaptation + 1):
-    with open(r"C:\Users\Кирилл\Desktop\freefem_test\DATA\adaptation " + str(i) +" .txt") as f:
+    with open(r".\DATA\adaptation " + str(i) +" .txt") as f:
         for line in f:
             dat.append([float(x) for x in line.split()])
+with open(r".\DATA\Exact.txt") as f:
+    for line in f:
+        dat.append([float(x) for x in line.split()])
 dat = np.array(dat, dtype=float)
 
 
@@ -23,6 +26,7 @@ fig.subplots_adjust(wspace=0.4, hspace=0.6, left=0.09, right=0.92, top=0.915, bo
 ax_1.plot(dat[0:nDots, 0], dat[0:nDots, 1], label="Without adaptation")
 for i in range(1, nAdaptation + 1):
     ax_1.plot(dat[nDots * i:nDots * (i + 1), 0], dat[nDots * i:nDots * (i + 1), 1], label="adaptation" + str(i))
+ax_1.plot(dat[nDots * (nAdaptation + 1):, 0], dat[nDots * (nAdaptation + 1):, 1], label="Exact solution")
 ax_1.set(xlabel='s')
 ax_1.set(ylabel='u(x(s),y(s))')
 ax_1.legend()
